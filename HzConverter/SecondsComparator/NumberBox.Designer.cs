7ab2df4c86591c0ba0b0ff71bf852a27e198ab1a
Christian <114274872+CristianLaimon@users.noreply.github.com>
2024-09-10 17:08:04 -0600

    Working Second Converter Component

---
+++ HzConverter/SecondsComparator/NumberBox.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace HzConverter
+{
+    partial class NumberBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Dock = DockStyle.Fill;
+            textBox1.Location = new Point(0, 0);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 23);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "0";
+            // 
+            // NumberBox
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(textBox1);
+            MinimumSize = new Size(0, 24);
+            Name = "NumberBox";
+            Size = new Size(125, 24);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+    }
+}
